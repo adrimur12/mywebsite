9828d902c123dca3e86fe25c910e61e56eb05c10
Adrian Muresan <adrian.muresan92@yahoo.com>
2023-06-03 17:11:56 +0100

    update

--- javascript/script.js
+++ javascript/script.js
@@ -1,12 +1,4 @@
 //Form validation Script
-// Get all the necessary DOM elements
-const form = document.getElementById('form');
-const username = document.getElementById('fullName');
-const email = document.getElementById('email');
-const message = document.getElementById('message');
-//const submitButton = document.querySelector('.submit')
-const successMessage = document.getElementById('form-submitted-msg');
-
 //setError function if is an empty string, which receive an html element and error message as parameter
 const setError = (element, message) => {
   //get parent element
@@ -38,104 +30,131 @@ const isValidEmail = email => {
   const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
 }
+//return true if the user provided a valid phone number
+const isValidPhoneNumber = phone => {
+  const re = /^[0-9\-]+$/;
+  return re.test(phone);
+}
 
+//validate inputs function
+const validateInputs = () => {
+
+  //validate inputs function
+  let submitForm;
+
+  //Initially set submitForm to true. 
+  submitForm = true;
+
+  //Retrieve variables to be validated and sanitized
+  let fullName = new String(document.ContactForm.fullName.value);
+  let email = new String(document.ContactForm.email.value);
+  let phone = new String(document.ContactForm.phone.value);
+  let message = new String(document.ContactForm.message.value);
+
+  //check whether is an empty string or has value
+  if (fullName.length < 1) {
+      //set an error if is an empty string
+      setError(document.ContactForm.fullName, 'Full name is required');
+      submitForm = false;
+  } else {
+      //set success if the username is given
+      setSuccess(document.ContactForm.fullName);
+  }
+
+  if (email.length < 1) {
+      setError(document.ContactForm.email, 'Email is required');
+      submitForm = false;
+      //check if is the valid email, if is not set an error
+  } else if (!isValidEmail(email)) {
+      setError(document.ContactForm.email, 'Provide a valid email address');
+      submitForm = false;
+  } else {
+      setSuccess(document.ContactForm.email);
+  }
+
+  if (phone.length < 1) {
+      setError(document.ContactForm.phone, 'Phone number is required');
+      submitForm = false;
+      //check if is the valid phone number, if is not set an error
+  } else if (phone.length !== 10) {
+      setError(document.ContactForm.phone, 'Phone number must be 10 Digits');
+  } else if (!isValidPhoneNumber(phone)) {
+      setError(document.ContactForm.phone, 'Provide a valid phone number');
+  } else {
+      setSuccess(document.ContactForm.phone);
+  }
+  //check if the message input is empty, if it is set an error
+  if (message.length < 1) {
+      setError(document.ContactForm.message, 'Message is required');
+      submitForm = false;
+  } else {
+      setSuccess(document.ContactForm.message);
+  }
+
+  if (submitForm == false) {
+      //The form cannot be submitted
+      return false;
+  } else {
+      //SANITIZE user inputs by allowing only [a-z 0-9 _ - . @] 
+      fullName = fullName.replace(/[^a-z0-9\s\-]/gim, "");
+      fullName = fullName.trim();
+      email = email.replace(/[^a-z0-9_@.-]/gim, "");
+      email = email.trim();
+      phone = phone.replace(/[^a-z0-9_@.-]/gim, "");
+      phone = phone.trim();
+      message = message.replace(/[^a-z0-9_@.-]/gim, "");
+      message = message.trim();
+      
+      //Firebase Object
+      const firebaseConfig = {
+            apiKey: "AIzaSyAeOl9jpIfBegWNUBgaoHo71I34u3qWOc8",
+            authDomain: "cloud-form-35994.firebaseapp.com",
+            projectId: "cloud-form-35994",
+            storageBucket: "cloud-form-35994.appspot.com",
+            messagingSenderId: "845134013506",
+            appId: "1:845134013506:web:82f0ca5181fbfd5ea6b9db"
+        };
+        
+        //Initialize Firebase
+        firebase.initializeApp(firebaseConfig);
+        let firestore = firebase.firestore();
+  
+        //Variable to access database collection
+        const db = firestore.collection("formData");
+        //Save Form Data To Firebase
+        db.doc()
+            .set({
+            fullName: fullName,
+            email: email,
+            phone: phone,
+            message: message
+        })
+            .then(() => { })
+            .catch((error) => {
+            console.log(error);
+        });
+
+        const successMessage = document.getElementById('form-submitted-msg');
+        //display the message if all the inputs are valid
+       successMessage.style.display = 'block';
+       //reset the form 
+       form.reset();
+           
+       setTimeout(() => {
+           successMessage.style.display = 'none';
+       }, 3000)
+    }
+}
 // Create function to handle the submit event on the form
 const handleSubmit = (e) => {
   //prevent default form for submission
   e.preventDefault();
 
-  //validate inputs function, to validate inputs
-  const validateInputs = () => {
-    //get value of all the inputs fields, used trim to remove all the white spaces that the string have
-    const usernameValue = username.value.trim();
-    const emailValue = email.value.trim();
-    const messageValue = message.value.trim();
-
-    //check whether is an empty string or has value
-    if (usernameValue === '') {
-      //set an error if is an empty string
-      setError(username, 'Username is required');
-      return false;
-    } else {
-      //set success if the username is given
-      setSuccess(username);
-    }
-
-    // if the email input is empty , set the error
-    if (emailValue === '') {
-      setError(email, 'Email is required');
-      return false;
-      //check if is the valid email, if is not set an error
-    } else if (!isValidEmail(emailValue)) {
-      setError(email, 'Provide a valid email address');
-      return false;
-    } else {
-      setSuccess(email);
-    }
-
-    //check if the message input is empty, if it is set an error
-    if (messageValue === '') {
-      setError(message, 'Message is required');
-      return false;
-    } else {
-      setSuccess(message);
-    }
-    return true;
-  };
-
-    if (validateInputs()) {
-      //invoke the data function to get the form submission
-      data();
-      //display the message if all the inputs are valid
-      successMessage.style.display = 'block';
-      //reset the form 
-      form.reset();
-
-      setTimeout(() => {
-        successMessage.style.display = 'none';
-      }, 3000)
-    }
+  validateInputs();    
 }
 
 //add event listnere to the form on the submit event
 form.addEventListener('submit', (e) => handleSubmit(e));
-
-//Firebase Object
-const firebaseConfig = {
-  apiKey: "AIzaSyAeOl9jpIfBegWNUBgaoHo71I34u3qWOc8",
-  authDomain: "cloud-form-35994.firebaseapp.com",
-  projectId: "cloud-form-35994",
-  storageBucket: "cloud-form-35994.appspot.com",
-  messagingSenderId: "845134013506",
-  appId: "1:845134013506:web:82f0ca5181fbfd5ea6b9db"
-};
-
-//Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-let firestore = firebase.firestore();
-
-//Variable to access database collection
-const db = firestore.collection("formData");
-
-//get form submission data
-const data = () => {
-
-  let fullNameInput = username.value;
-  let emailInput = email.value;
-  let messageInput = message.value;
-
-//Save Form Data To Firebase
-  db.doc()
-  .set({
-    fullName: fullNameInput,
-    email: emailInput,
-    message: messageInput,
-  })
-  .then(() => { })
-  .catch((error) => {
-    console.log(error);
-  });
-};
 
 //adjust text area for message on contact form
 const textarea = document.querySelector('textarea')
